2e6c5f2cc2ebe9781bbfa4794f39602076bf941b
byFluerash <fluerashdaniil@gmail.com>
2020-07-31 23:54:44 +0400

    Add all need routes

--- F_app/routes.py
+++ F_app/routes.py
@@ -9,5 +9,25 @@ def home():
 
 @app.route('/cat')
 def cat():
-    user = {'username': 'Miguel'}
+    user = {'username': 'CAT'}
     return render_template('cat.html', title='Home', user=user)
+
+@app.route('/dog')
+def dog():
+    user = {'username': 'DOG'}
+    return render_template('dog.html', title='Home', user=user)
+
+@app.route('/fox')
+def fox():
+    user = {'username': 'FOX'}
+    return render_template('fox.html', title='Home', user=user)
+
+@app.route('/history')
+def history():
+    user = {'username': 'History'}
+    return render_template('history.html', title='Home', user=user) 
+
+@app.route('/history/static/<uuid>')
+def history_static():
+    user = {'username': 'History-Static-number'}
+    return render_template('history_static_number.html', title='Home', user=user)  
